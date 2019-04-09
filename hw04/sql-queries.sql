40d0dfb36c54488afc9ab920d3a979239c5cfd8e
David Herel <hereldav@fel.cvut.cz>
2019-04-09 11:22:43 +0200

    hw04 done

--- hw04/sql-queries.sql
+++ hw04/sql-queries.sql
@@ -1,40 +1,26 @@
---REQ
 
---    Natural join, generic inner join, as well as outer join
---    Grouping and aggregation (both GROUP BY and HAVING clauses)
---    One set operation: UNION, INTERSECT or EXCEPT
---    Sorting (ORDER BY clause)
---    Sub-query expression
-
--- 
--- 
-
---ORDER BY completed, GROUP BY, HAVING, EXCEPT completed
 --find all employyes names that have higher salary than 30 000CZK in descending order
 SELECT E.wage, E.name
 FROM Employee as E
-GROUP BY (E.wage)
+GROUP BY E.wage, E.name
 HAVING (E.wage >= 30000)
-ORDER BY E.wage DESC
+ORDER BY E.wage DESC;
 
 --Name of customers, who did not bought any membership from 2018
---Sub-query completed - probably
 SELECT Customer.name
 FROM Customer
 WHERE Customer.id NOT IN
 (SELECT Membership.customer
 FROM Membership
-WHERE startDate > "2018-1-1");
+WHERE startDate > '2018-1-1');
 
 --Name of customers who never bought any membership
---left join
 SELECT Customer.name
 FROM Customer
 LEFT JOIN Membership
 ON Customer.id = Membership.customer
-WHERE startDate IS NULL
+WHERE startDate IS NULL;
 
---Except used and natural join too
 --Choosing equipment which is used more than 30 minutes
 SELECT equipment
 FROM usingNow
@@ -42,11 +28,10 @@ EXCEPT
 SELECT equipment
 FROM usingNow
 NATURAL JOIN Customer
-WHERE length < 30
+WHERE length < 30;
 
---Inner join completed
 --find city for customer who has id 1
 SELECT city
 FROM CustAdress
 JOIN Customer ON Customer.id = CustAdress.customer
-WHERE id = 1
+WHERE id = 1;
